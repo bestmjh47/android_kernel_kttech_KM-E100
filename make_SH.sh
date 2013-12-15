d177ccda3a414a6a122220ed3b13b7d8eb808512
Sunghun Ra <sktjdgns1189@naver.com>
2013-12-15 19:30:34 +0900

    Add defconfig and clean up

---
+++ make_SH.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#############################
+# Take LTE SH Kernel Source #
+# by SungHun.Ra             #
+# http://theteamdev.com     #
+#############################
+make say_defconfig
+make -j8
